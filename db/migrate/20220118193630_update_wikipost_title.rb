57bdf8cf1c16382915fa99560c205f0253fd8200
Cecilia Quintana <20249591+ceciquin@users.noreply.github.com>
2022-01-26 15:11:56 -0300

    fixing typo in db migration

--- db/migrate/20220118193630_update_wikipost_title.rb
+++ db/migrate/20220118193630_update_wikipost_title.rb
@@ -1,8 +1,8 @@
 class UpdateWikipostTitle < ActiveRecord::Migration[6.1]
   def up
-    change_column :wikipost, :title, :string, :limit => 50
+    change_column :wikiposts, :title, :string, :limit => 50
   end
   def down
-    change_column :wikipost, :title, :string, :limit => 255
+    change_column :wikiposts, :title, :string, :limit => 255
   end
 end
